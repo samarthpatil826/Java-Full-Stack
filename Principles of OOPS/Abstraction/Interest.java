06a695f6a7203802b99940c5f75ee1227e475e49
Samarth B Patil <144235167+samarthpatil826@users.noreply.github.com>
2025-06-15 23:59:14 +0530

    Delete Principles of OOPS/Abstraction directory

--- Principles of OOPS/Abstraction/Interest.java
+++
@@ -1,9 +0,0 @@
-package abstraction;
-
-public abstract class Interest {
-	
-	public abstract void simpleInterest(double p, double r, int t);
-	
-	public abstract void compoundInterest(double p, double r, int n, int t);
-
-}
