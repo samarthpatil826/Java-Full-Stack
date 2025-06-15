33a63fdb3cc00d7ea631778550c458dcebca741f
Samarth B Patil <144235167+samarthpatil826@users.noreply.github.com>
2025-06-16 00:00:41 +0530

    Add files via upload

---
+++ Principles of OOPS/Abstraction/CalculatorImp.java
@@ -0,0 +1,19 @@
+package abstraction;
+
+public class CalculatorImp extends Calculator{
+	
+	public void addition(int a, int b) {
+		
+		System.out.println(a+b);
+	}
+	
+	public void subtraction(int a, int b) {
+		
+		System.out.println(a-b);
+	}
+	
+	public void multiplication(int a, int b) {
+		System.out.println(a*b);
+	}
+
+}
