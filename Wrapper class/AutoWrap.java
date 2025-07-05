e4f97e7e8ab85554e9305c5dda7b58116be3f0ff
Samarth B Patil <144235167+samarthpatil826@users.noreply.github.com>
2025-07-05 16:19:23 +0530

    Add files via upload

---
+++ Wrapper class/AutoWrap.java
@@ -0,0 +1,14 @@
+package wrapper;
+
+public class AutoWrap {
+
+	public static void main(String[] args) {
+		
+		Integer i=30;    //auto-boxing
+		System.out.println(i);
+		
+		int a=i;
+		System.out.println(a);     //auto-unboxing
+	}
+
+}
